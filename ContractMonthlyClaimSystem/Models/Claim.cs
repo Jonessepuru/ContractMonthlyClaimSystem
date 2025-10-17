487050309029a81750e05f375e15c2aef7004ea4
JONES <JONES@JONES-SEPURU>
2025-10-17 12:40:44 +0200

    Add project files.

---
+++ ContractMonthlyClaimSystem/Models/Claim.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ContractMonthlyClaimSystem.Models
+{
+    public class Claim
+    {
+        public int ClaimID { get; set; }
+        public int LecturerID { get; set; }
+        public string LecturerName { get; set; } // For display
+        public decimal Hours { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime SubmissionDate { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ ContractMonthlyClaimSystem/Models/Lecturer.cs
@@ -0,0 +1,10 @@
+﻿namespace ContractMonthlyClaimSystem.Models
+{
+    public class Lecturer
+    {
+        public int LecturerID { get; set; }
+        public string Name { get; set; }
+        public decimal TotalHours { get; set; }
+        public decimal TotalPaid { get; set; }
+    }
+}
